ca7bd0b7370e9773e721f725083a1d937b61a632
NoahForTW <111335518+NoahForTW@users.noreply.github.com>
2025-02-06 14:37:47 +0800

    beta build test 3

--- Build/HTML5Web.framework.js
+++ Build/HTML5Web.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  5372016: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 5372077: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 5372141: function() {return Module.webglContextAttributes.powerPreference;},  
- 5372199: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 5372254: function($0) {performance.now = function() { return $0; };},  
- 5372302: function($0) {performance.now = function() { return $0; };},  
- 5372350: function() {performance.now = Module['emscripten_get_now_backup'];}
+  5373808: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 5373869: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 5373933: function() {return Module.webglContextAttributes.powerPreference;},  
+ 5373991: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 5374046: function($0) {performance.now = function() { return $0; };},  
+ 5374094: function($0) {performance.now = function() { return $0; };},  
+ 5374142: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
